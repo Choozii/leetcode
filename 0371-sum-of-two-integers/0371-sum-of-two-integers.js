577f8523d14bffef2ede699653e9ba9173d2435c
추지예 <74968456+Choozii@users.noreply.github.com>
2023-09-16 13:58:47 +0900

    Time: 47 ms (78.01%), Space: 41.4 MB (75.28%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    
+    while(b){
+        let carry = a & b;
+    
+        a = a ^ b;    
+        b = carry << 1;
+    }
+    
+    return a;
+};
