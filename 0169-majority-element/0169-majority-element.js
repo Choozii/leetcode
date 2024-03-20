83994b695896ef8d3507542b0c494359317166f2
추지예 <74968456+Choozii@users.noreply.github.com>
2024-03-20 16:53:03 +0900

    Time: 58 ms (66.14%), Space: 52.1 MB (45.65%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const map = {};
+    
+    for(let i=0;i<nums.length;i++){
+        map[nums[i]] = map[nums[i]]?map[nums[i]]+1:1;
+        if(map[nums[i]] > nums.length/2){
+            return nums[i];
+        }
+    }
+};
