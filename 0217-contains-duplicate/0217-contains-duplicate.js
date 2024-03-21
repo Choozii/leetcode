089b63e9259bdd93bda41452775445000eda6ca7
추지예 <74968456+Choozii@users.noreply.github.com>
2024-03-21 12:43:23 +0900

    Time: 84 ms (43.74%), Space: 61 MB (74.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const map = new Map();
-    
+    const map={}
     for(let i=0;i<nums.length;i++){
-        if(map.get(nums[i])){
+        if(map[nums[i]]){
             return true;
         }
-        
-        map.set(nums[i], 1);
+        map[nums[i]] = 1;
     }
     return false;
 };
