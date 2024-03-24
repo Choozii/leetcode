4b5235d4663795dbc4b672ed994f3405bbdbc6d4
추지예 <74968456+Choozii@users.noreply.github.com>
2024-03-24 16:52:00 +0900

    Time: 65 ms (5.70%), Space: 48.7 MB (45.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,13 +4,14 @@
  */
 var climbStairs = function(n) {
     const dp = new Array(n+1);
+    
     dp[0] = 0;
     dp[1] = 1;
     dp[2] = 2;
     
-    for(let i=3;i<n+1;i++){
-        dp[i] = dp[i-1] + dp[i-2];
+    for(let i=3;i<=n;i++){
+        dp[i] = dp[i-2] + dp[i-1];        
     }
-    
+        
     return dp[n];
 };
