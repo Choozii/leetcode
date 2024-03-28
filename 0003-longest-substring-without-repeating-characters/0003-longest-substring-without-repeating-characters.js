645131b3d7950ee700a08738d1505562df10e416
추지예 <74968456+Choozii@users.noreply.github.com>
2024-03-28 13:55:33 +0900

    Time: 60 ms (99.65%), Space: 55.2 MB (40.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,20 +3,35 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let maxLen = 0;
+    /*
+    input : string s
+    output : 반복 없는 longest substring length 
+    
+    s = "abcabcbb"
+         ---
+          ---
+           ---
+            ---
+              --
+    
+    substring이라는 변수에 * 지금까지 없었던 char이라면 추가*
+    maxLength에 Math.max로 계속 저장
+    - 만약에 있는 char이라면?
+      - n번째 글자와 겹친다면, 기존 n번째까지 삭제하고 새로운 글자 넣어버리기
+    */
+    
+    let substring = "";
+    let maxLength = 0;
     
     for(let i=0;i<s.length;i++){
-        const subSet = new Set();
-        
-        for(let j=i;j<s.length;j++){
-            if(!subSet.has(s[j])){
-                subSet.add(s[j]);        
-            }else{
-                break;
-            }
+        if(!substring.includes(s[i])){
+            substring += s[i];
+        }else{
+            const idx = substring.indexOf(s[i]);
+            substring = substring.slice(idx+1, substring.length) + s[i];
         }
-        
-         maxLen = Math.max(maxLen, subSet.size);
+        maxLength = Math.max(substring.length, maxLength);
     }
-    return maxLen;
+    
+    return maxLength;
 };
