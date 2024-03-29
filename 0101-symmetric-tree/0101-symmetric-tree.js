3c81306a701361abe1714638a7fe17fd83c57940
추지예 <74968456+Choozii@users.noreply.github.com>
2024-03-29 18:25:14 +0900

    Time: 72 ms (9.61%), Space: 52.9 MB (5.18%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+   const q = [root];
+ 
+    while(q.length > 0){
+        const len = q.length;
+        const temp=[];
+        
+        for(let i=0;i<len;i++){
+            const curr = q.pop();
+            
+            temp.push(curr.left || null);
+            temp.push(curr.right || null);
+        }
+        
+        
+        for(let i=0;i<temp.length/2;i++){
+            if(!temp[i] && !temp[temp.length-1-i]){
+                continue;
+            }if(!temp[i] || !temp[temp.length-1-i]){
+                return false;
+            }
+            
+             if(temp[i].val !== temp[temp.length-1-i].val){
+                return false;
+             }
+        }
+        
+        q.push(...temp.filter(node => node));
+    }
+    
+    return true;
+    
+};
