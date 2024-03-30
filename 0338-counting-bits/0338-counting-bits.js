91b9a71c12a268445542c18f3f1e11b385da1e9d
추지예 <74968456+Choozii@users.noreply.github.com>
2024-03-30 22:48:02 +0900

    Time: 71 ms (71.63%), Space: 55.2 MB (85.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -2,30 +2,17 @@
  * @param {number} n
  * @return {number[]}
  */
-
-
-const isPwoOfTwo = (number) => {
-    return !(number & (number-1))
-}
 var countBits = function(n) {
-    if(n === 0){
-        return [0];
+    const arr = [];
+    arr[0] = 0;
+    
+    for(let i=1;i<=n;i++){
+        let number = i;
+       while(number !== 0){
+            number &= number-1;
+           arr[i] = arr[i]?arr[i]+1:1;
+        }  
     }
     
-    const dp = new Array(n+1);
-    dp[0] = 0;
-    dp[1] = 1;
-    
-    let closest = 0;
-    
-    for(let i=2;i<=n;i++){
-        if(isPwoOfTwo(i)){ 
-            dp[i] = 1;
-            closest = i;
-        }else{
-            dp[i] = dp[closest] + dp[i-closest];
-        }
-    }
-    
-    return dp;
+    return arr;
 };
