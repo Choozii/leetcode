9668a818578728edf286a4d89b76efb3bdac64b5
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-04-01 16:01:05 +0900

    Time: 51 ms (94.39%), Space: 52 MB (72.49%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -5,13 +5,11 @@
 var evalRPN = function(tokens) {
     const isToken = (char) => ['+','-','*','/'].includes(char);
     const stack = [];
-
     let curr = 0;
     
     while(curr < tokens.length){
         if(!isToken(tokens[curr])){
-            stack.push(tokens[curr]);
-            curr++;
+            stack.push(tokens[curr++]);
             continue;
         }   
         
@@ -19,24 +17,20 @@ var evalRPN = function(tokens) {
         const num2 = Number(stack.pop());
         const num1 = Number(stack.pop());
         
-        let result;
-        
         switch(operator){
             case "+" :
-                result = num1 + num2;
+                stack.push(num1 + num2);
                 break;
             case "-" : 
-                result = num1 - num2;
+                stack.push(num1 - num2);
                 break;
             case "*" : 
-                result = num1 * num2;
+                stack.push(num1 * num2);
                 break;
             case "/" : 
-                result = Math.trunc(num1 / num2);
+                stack.push(Math.trunc(num1 / num2));
                 break;
         }
-        
-        stack.push(result);
         curr++;
     }
         
