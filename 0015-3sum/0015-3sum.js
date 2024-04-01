2225aaa3e5f0fb50f96014227bf554f42766b421
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-04-01 10:33:58 +0900

    Time: 169 ms (42.03%), Space: 65.1 MB (69.46%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+    const ans = [];
+
+    const check = (idx) => {
+        const number = nums[idx];
+        let left = idx+1, right = nums.length-1;
+        
+        while(left < right){   
+            const sum = number+nums[left]+nums[right];
+            
+            if(sum < 0){
+                left++;
+            }else if(sum > 0){
+                right--;
+            }else{
+                ans.push([number, nums[left++], nums[right--]]);
+                
+                while(nums[left] === nums[left-1]){
+                    left++;
+                }
+                while(nums[right] === nums[right+1]){
+                    right--;
+                }
+            }
+            
+        }
+    }
+        
+        
+    
+    nums.sort((a,b) => a-b);
+    
+    nums.forEach((val, idx) => {
+        if(idx === 0 || nums[idx-1] !== nums[idx]){ // 중복되는 값은 호출되지 않도록 처리
+          check(idx);               
+        }
+    });
+    
+
+    
+    return ans;
+    
+};
