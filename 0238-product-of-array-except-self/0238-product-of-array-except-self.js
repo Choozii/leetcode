feb9409d0a14baf96eb222ab68f478eb81990f4e
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-04-05 16:11:45 +0900

    Time: 92 ms (46.97%), Space: 64.9 MB (13.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,25 +2,28 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+/*
+input: integer array nums
+output : answer[i] = nums[i]만 제외한 다른 요소들의 곱으로 이루어진 answer array
+
+*/
 var productExceptSelf = function(nums) {
-    const prev = [];
-    let prev_number = 1;
-    for(let i=0;i<=nums.length-1;i++){
-        prev.push(prev_number);
-        prev_number *= nums[i];
+    const product = new Array(nums.length);
+    const left = new Array(nums.length).fill(1);
+    const right = new Array(nums.length).fill(1);
+    
+    for(let i=1;i<nums.length;i++){
+        left[i] = left[i-1] * nums[i-1];
+    }
+
+    for(let i=nums.length-2;i>=0;i--){
+        right[i] = right[i+1] * nums[i+1];
     }
     
-    const next = new Array(nums.length);
-    let next_number = 1;
-    for(let i = nums.length-1; i>=0;i--){
-        next[i] = next_number;
-        next_number *= nums[i];
+    for(let i=0;i<nums.length;i++){
+        product[i] = left[i] * right[i];
     }
     
-    const result = [];
-    for(let i=0;i<next.length;i++){
-        result.push(prev[i]*next[i]);
-    }
-    
-    return result;
+    return product;
 };
