823ba5f6b81d5f3cdf189a39ffd0d5b25a69a255
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-04-07 10:31:30 +0900

    Time: 88 ms (62.21%), Space: 61.4 MB (49.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,28 +2,19 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-
-/*
-input: integer array nums
-output : answer[i] = nums[i]만 제외한 다른 요소들의 곱으로 이루어진 answer array
-
-*/
 var productExceptSelf = function(nums) {
-    const product = new Array(nums.length);
-    const left = new Array(nums.length).fill(1);
-    const right = new Array(nums.length).fill(1);
+    
+    let prev = 1;
+    const output = new Array(nums.length).fill(1);
     
     for(let i=1;i<nums.length;i++){
-        left[i] = left[i-1] * nums[i-1];
+        output[i] = output[i-1] * nums[i-1];
     }
 
     for(let i=nums.length-2;i>=0;i--){
-        right[i] = right[i+1] * nums[i+1];
+        prev *= nums[i+1];
+        output[i] *= prev; 
     }
-    
-    for(let i=0;i<nums.length;i++){
-        product[i] = left[i] * right[i];
-    }
-    
-    return product;
+
+    return output;
 };
