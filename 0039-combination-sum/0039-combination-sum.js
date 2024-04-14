740bd05def0b2115d7d62737897fba416be08b7f
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-04-14 13:35:22 +0900

    Time: 78 ms (51.07%), Space: 57.7 MB (23.77%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -6,24 +6,22 @@
 var combinationSum = function(candidates, target) {
     const output = [];
     
-    const helper = (combinations, sum) => {
+    const helper = (combinations, sum, prev) => {
         if(sum < 0){
             return;
         }else if(sum === 0){
-            // 겹치는 값이 있는지 확인
-            const isUnique = output.every(value => value.sort((a,b)=>a-b).join("") !== combinations.sort((a,b) => a-b).join(""));
-            if(isUnique){
-                output.push(combinations);
-            }
+            output.push(combinations);
             return;
         }
         
         for(let i=0;i<candidates.length;i++){
-            helper([...combinations, candidates[i]], sum-candidates[i]);            
+            if(prev <= candidates[i]){
+                helper([...combinations, candidates[i]], sum-candidates[i], candidates[i]);           
+            }
         }
     }
     
-    helper([], target);
+    helper([], target, 0);
     
     return output;
 };
