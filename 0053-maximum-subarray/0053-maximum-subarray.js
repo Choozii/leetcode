f68b81a46beb41845074bce47eb4006296ce82d0
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-04-16 14:19:26 +0900

    Time: 81 ms (18.31%), Space: 64.3 MB (5.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    const dp = new Array(nums.length);
-    dp[0] = nums[0];
+    const optimal = [nums[0]];
     
     for(let i=1;i<nums.length;i++){
-        dp[i] = Math.max(nums[i], nums[i]+dp[i-1])
+        optimal[i] = Math.max(optimal[i-1]+nums[i], nums[i]);
     }
     
-    return Math.max(...dp);
+    return Math.max(...optimal);
 };
