a34f14dc0c4503891e98eacd0e921eb72abcf702
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-05-03 18:01:03 +0900

    Time: 68 ms (53.65%), Space: 57.2 MB (47.46%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+    
+    while(left<right){
+        const width = right - left;
+        maxArea = Math.max(maxArea, Math.min(height[left], height[right])*width);
+        
+        if(height[left] <= height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    
+    return maxArea;
+};
