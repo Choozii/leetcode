003f79e5763fcba2f74ab01520caab0a9a306e6e
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-05-07 10:35:50 +0900

    Time: 83 ms (7.64%), Space: 55.5 MB (19.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,27 +2,34 @@
  * @param {string} s
  * @return {boolean}
  */
-
-/*
-input : 문자열
-output : palindrome인지 여부
-규칙 : 대문자 -> 소문자로 변환, 알파벳이 아닌 문자 제거 후 뒤집어도 똑같으면 회문
-*/
 var isPalindrome = function(s) {
-    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let left = 0, right = s.length-1;
     
-    const newStr = [...s.toLowerCase()].filter(char => char!==" " && (alphabet.includes(char) || Number.isInteger(char*1)));
+    const isValid = (input) => {
+        const regExp = new RegExp(/^[a-z0-9]/i);
+        return regExp.test(input);
+    }
+    
+    while(left <= right){
         
-    if(!newStr.length){
-        return true;
+        const leftChar = s[left].toLowerCase();
+        const rightChar = s[right].toLowerCase();
+        
+        if(!isValid(leftChar)){
+            left++;
+            continue;
+        }
+        if(!isValid(rightChar)){
+            right--;
+            continue;
+        }
+        
+        if(leftChar === rightChar){
+            left++;
+            right--;
+        }else{
+            return false;
+        }
     }
-    
-    for(let i=0;i<newStr.length/2;i++){
-        if(newStr[i] !== newStr[newStr.length-1-i]){
-           return false;
-           }
-    }
-    
     return true;
-    
 };
