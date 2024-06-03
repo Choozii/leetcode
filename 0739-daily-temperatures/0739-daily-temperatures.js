5e40033659631765dc3317e7d6dc391b09281e13
Jiye Choo <74968456+Choozii@users.noreply.github.com>
2024-06-03 13:31:59 +0900

    Time: 260 ms (10.32%), Space: 71.8 MB (88.49%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -2,28 +2,35 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {    
-    const stack = [[temperatures[temperatures.length-1], temperatures.length-1]];
-    const output = [0];
+var dailyTemperatures = function(temperatures) {
+    const ans = [];
+    const stack = [];
     
-    for(let i=temperatures.length-2;i>=0;i--){
-        while(stack.length>0){
-            const [val, idx] = stack[stack.length - 1];
-            
-                if(val <= temperatures[i]){
-                    stack.pop();
-                }else{
-                    stack.push([temperatures[i], i]);
-                    output.push(idx-i);
-                    break;
-                }      
-        }
-        
+    for(let i=temperatures.length-1;i>=0;i--){
         if(stack.length === 0){
             stack.push([temperatures[i], i]);
-            output.push(0);
+            ans[i] = 0;
+            continue;
+        }
+        
+        while(true){
+            if(stack.length === 0){
+                ans[i] = 0;
+                stack.push([temperatures[i], i]);
+                break;
+            }
+            
+            const [stackValue, stackIdx] = stack[stack.length-1];
+            
+            if(stackValue <= temperatures[i]){
+                stack.pop();
+            }else{
+                ans[i] = stackIdx - i;
+                stack.push([temperatures[i], i]);
+                break;
+            }    
         }
     }
     
-    return output.reverse();
+    return ans;
 };
